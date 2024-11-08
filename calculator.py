adb5632d2b1d9aaf6e75088675e4a10ed1f54eaa
ohmmyrisingstar <mxpeeranut@gmail.com>
2024-11-08 04:47:37 +0000

    Finished

--- calculator.py
+++ calculator.py
@@ -3,23 +3,27 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
-    def multiply(self, a, b):
+    def multiply(self, a, b): 
         result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
+        if (a and b >= 0) or (a and b <= 0):
+            for i in range((abs(b))):
+                result = self.add(result, abs(a)) 
+        
         return result
 
-    def divide(self, a, b):
+    def divide(self, a, b): 
+        if b == 0:
+            return("Eror")
         result = 0
-        while a > b:
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
         return result
     
     def modulo(self, a, b):
-        while a <= b:
+        while a >= b:
             a = a-b
         return a
 
--- test_calculator.py
+++ test_calculator.py
@@ -8,8 +8,30 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(5,-5 ),0)
+
 
     # Add the following test methods to the TestCalculator class:
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(2, 1), 1)
+        self.assertEqual(self.calc.subtract(-10, -1), -9)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3),6)
+        self.assertEqual(self.calc.multiply(-2,-3),6)    
+        self.assertEqual(self.calc.multiply(0, 0),0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2),5)
+        self.assertEqual(self.calc.divide(20, 2),10)
+
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(10,2),0)
+        self.assertEqual(self.calc.modulo(15,2),1)
+
+
 
 if __name__ == '__main__':
     unittest.main()
